9e48be73a17dd5f5f10b06dda88cd917d7ab7975
andresegas <124941015+andresegas@users.noreply.github.com>
2023-03-28 21:16:31 -0500

    .bak

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+USE Enfermedades_respiratorias;
+
+CREATE TABLE Usuario(
+   cedula INT NOT NULL,
+   nombre VARCHAR(255) NOT NULL,
+   descripcion VARCHAR(255) NOT NULL
+   PRIMARY KEY (cedula),
+   edad int not null,
+   correo varchar(255) NOT NULL,
+);
+
+
+
+
+
